077baaa1d10c9e9be977ceb18bb46ce594c8dea8
Manny Aguilar <mannyaaguilar@yahoo.com>
2018-01-10 10:29:35 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner login = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String inputFromUser = login.nextLine();
+
+        if(inputFromUser.equals ("Manny"));{
+            System.out.println("Hello " + inputFromUser);}
+        }
+    }
+
